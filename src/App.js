8443d0e2c5971121a9a233ea53f1638e1fb66245
mshaheeruddin <mshaheeruddin19757@gmail.com>
2023-07-16 03:49:56 +0500

    meetup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 
-import Todo from "./components/Todo";
-
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text = "Learn React" />
-      <Todo text = "Learn Vue" />
-      <Todo text = "Learn Angular" />
-     
+      
     </div>
   );
 }
